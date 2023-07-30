529bfcfeae3a499269b426e7c121003fbb966f1c
Ashish Desai <desai.ashish76@gmail.com>
2023-07-30 19:10:15 -0400

    added fetch method to get list of todos

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  state = {
+    todos: [],
+    // name: '',
+    // completed: false
+  }
+  fetchToDos = () => {
+    axios.get(`${URL}`).then(res => {
+      this.setState({todos: res.data.data});
+      console.log(this.state.todos);
+    })
+  }
+  componentDidMount(){
+    console.log('component did mount')
+    this.fetchToDos();
+  }
   render() {
-    return null
+    return (
+      <div>
+        <div id='error'>Error: No error(s)</div>
+        <div id='todos'>
+          <h2>Todos:</h2>
+          <div>Walk the dog</div>
+          <div>Learn react</div>
+        </div>
+        <form id='todoForm'>
+          <input type='text' placeholder='Type todo' />
+          <input type='submit'/>
+          <button>Clear Complete</button>
+        </form>
+      </div>
+    )
   }
 }
+
