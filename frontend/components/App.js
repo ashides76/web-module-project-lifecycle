8a74256c18c9f4ef6aba2cb436236a37c811238b
Ashish Desai <desai.ashish76@gmail.com>
2023-07-30 21:11:37 -0400

    updated error state

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -6,13 +6,13 @@ const URL = 'http://localhost:9000/api/todos'
 export default class App extends React.Component {
   state = {
     todos: [],
-    // name: '',
-    // completed: false
+    error: '',
   }
   fetchToDos = () => {
     axios.get(URL).then(res => {
       this.setState({...this.state, todos: res.data.data});
-      console.log(this.state.todos);
+    }).catch(err => {
+      this.setState({...this.state, error: err.response.data.message})
     })
   }
   componentDidMount(){
@@ -22,7 +22,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <div id='error'>Error: No error(s)</div>
+        <div id='error'>Error: {this.state.error}</div>
         <div id='todos'>
           <h2>Todos:</h2>
           {this.state.todos.map(todo => {
